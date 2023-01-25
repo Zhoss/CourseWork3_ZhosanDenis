ee9b034568fb3f0c293da20e3645ec8c3fcd4a46
Zhoss <joss.90@mail.ru>
2023-01-25 10:13:08 +1000

    Initial commit

---
+++ src/main/java/pro/sky/java/course3/coursework3_zhosandenis/CourseWork3ZhosanDenisApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.coursework3_zhosandenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork3ZhosanDenisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork3ZhosanDenisApplication.class, args);
+    }
+
+}
